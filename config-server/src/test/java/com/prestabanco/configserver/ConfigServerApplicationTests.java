bad78e8fb27bd2d75fc598dd0e71584bc60c6b5c
Mohamed Al-Marzuk <mohamed.al-marzuk@usach.cl>
2024-12-26 21:36:52 -0300

    a

---
+++ config-server/src/test/java/com/prestabanco/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prestabanco.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
